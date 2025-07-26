07101e0d929cc99236fbc542c75d5b2660309368
Jeyanth Mandava <jayanth16994@gmail.com>
2025-07-26 09:44:46 +0000

    Un selected ones should be grey, not neon in the footer, like I attached

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -25,7 +25,7 @@ export function BottomNav() {
             key={item.href}
             href={item.href}
             className={cn(
-              'flex flex-col items-center gap-1 p-2 text-primary/70 transition-colors',
+              'flex flex-col items-center gap-1 p-2 text-gray-400 transition-colors',
               pathname.startsWith(item.href) && 'text-primary'
             )}
           >
@@ -33,7 +33,7 @@ export function BottomNav() {
             <span className="text-xs font-medium">{item.label}</span>
           </Link>
         ))}
-        <div className="flex flex-col items-center gap-1 p-2 text-primary/70 [&>div]:h-6 [&>div]:w-6 [&_[data-slot=avatar]]:h-6 [&_[data-slot=avatar]]:w-6">
+        <div className="flex flex-col items-center gap-1 p-2 text-gray-400 [&>div]:h-6 [&>div]:w-6 [&_[data-slot=avatar]]:h-6 [&_[data-slot=avatar]]:w-6">
            <ProfileMenu />
            <span className="text-xs font-medium">Profile</span>
         </div>
